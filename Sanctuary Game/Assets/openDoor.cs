26ef88011f3181ddeab6aa622b421faf2e3e4a3f
mquach <mquach@knights.ucf.edu>
2014-02-27 19:34:25 -0500

    cabin door animation added
    added the cabin door opening animation using a trigger script. Opens
    when you click with the mouse button, but needs to be changed to be
    opened only with the key

---
+++ Sanctuary Game/Assets/openDoor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openDoor : MonoBehaviour {
+	public Collider thePlayer;
+	public bool open;
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = null;
+		open = false;
+		this.animation ["cabinDoorAnim"].wrapMode = WrapMode.ClampForever;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open == true) {
+		this.animation.Play("cabinDoorAnim");
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("working");
+		if (other.gameObject.tag == "Player") {
+			thePlayer = other;
+		}
+	}
+
+	void OnTriggerStay(){
+		if (thePlayer != null) {
+			if (Input.GetMouseButtonDown (0)) {
+				open = true;
+			}
+		}
+	}
+}
