cec9a1b4b0b53b5a4a4c180fb3f157b7af66c143
mquach <mquach@knights.ucf.edu>
2014-02-28 20:05:29 -0500

    Moving platforms
    Made it so that some of the platforms for level one moves. Vertical ones
    are a little erratic though.

---
+++ Sanctuary Game/Assets/movePlatforms.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlatforms : MonoBehaviour {
+
+	// Sets animations to continously loop
+	void Start () {
+		this.animation.wrapMode = WrapMode.Loop;
+	}
+	
+	// Play the animations
+	void Update () {
+		this.animation.Play();
+	}
+}
