020e6b0da3eb08f419edf37dc47bfd4c17f8fc06
mquach <mquach@knights.ucf.edu>
2014-03-28 17:17:47 -0400

    Cabin vanishing

---
+++ Sanctuary Game/Assets/Scripts/CabinVanish.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CabinVanish : MonoBehaviour {
+	public Collider playerChar;
+	public GameObject shack;
+	public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+		playerChar = null;
+		shack = GameObject.Find ("Cabin");
+		door = GameObject.Find ("Door 1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider Other){
+		Debug.Log ("in");
+		if(Other.gameObject.tag == "Player"){
+			playerChar = Other;
+		}
+	}
+
+	void OnTriggerExit(Collider Other){
+		Debug.Log ("exit");
+		shack.SetActive (false);
+		door.SetActive (false);
+	}
+}
--- Sanctuary Game/Assets/Scripts/movePlatforms.cs
+++ Sanctuary Game/Assets/Scripts/movePlatforms.cs
@@ -6,6 +6,10 @@ public class movePlatforms : MonoBehaviour {
 	// Sets animations to continously loop
 	void Start () {
 		this.animation.wrapMode = WrapMode.Loop;
+		animation ["horPlatform"].speed = 0.5f;
+		animation ["horPlatform2"].speed = 0.8f;
+		animation ["vertPlatform"].speed = 0.8f;
+		animation ["vertPlatform2"].speed = 0.8f;
 	}
 	
 	// Play the animations
