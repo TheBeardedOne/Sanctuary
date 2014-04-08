e6d95dd9a62fd6ac45544241ec53715866ad6723
TheBeardedOne <jeremy.boggs42@gmail.com>
2014-04-07 20:21:06 -0400

    Literally only changed one script. Git thinks I changed like 1300 items.

--- Sanctuary Game/Assets/Scripts/playerInteract.cs
+++ Sanctuary Game/Assets/Scripts/playerInteract.cs
@@ -127,7 +127,7 @@ public class playerInteract: MonoBehaviour {
 			
 			//Change the final int to change distance from camera
 			hitObject.transform.position = (this.transform.position + (this.transform.forward * 2.25f)) - new Vector3(0, 0.5f, 0); 
-			//hitObject.transform.rotation = this.transform.rotation; 
+			hitObject.transform.rotation = this.transform.rotation; 
 		}
 		//If the object should not be held, detach from the camera and turn on gravity
 		else if(hitObject != null && isHolding == false){ 
@@ -152,7 +152,7 @@ public class playerInteract: MonoBehaviour {
 		
 		//Change the final int to change distance from camera
 		hitObject.transform.position = (this.transform.position + (this.transform.forward * 2.25f)) - new Vector3(0, 0.5f, 0); 
-		//hitObject.transform.rotation = this.transform.rotation; 
+		hitObject.transform.rotation = this.transform.rotation; 
 
 	}
 
