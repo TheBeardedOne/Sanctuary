d8db565a47d89006612d048eb15b0f719d4f1216
TheBeardedOne <jeremy.boggs42@gmail.com>
2014-04-10 16:50:28 -0400

    Letter pick up works. Reticule still needs to change when hovering over it.

---
+++ Sanctuary Game/Assets/letterBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class letterBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<dfSprite> ().IsVisible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (gameObject.GetComponent<dfSprite> ().IsVisible) {
+				if(Input.GetMouseButtonDown(0)){
+				gameObject.GetComponent<dfSprite> ().IsVisible = false;
+					}
+				}
+	}
+}
